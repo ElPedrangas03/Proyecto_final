cffefa973b2bf8dee6a18f06f77897f127f830e6
LizMDGav <lizmgav@gmail.com>
2024-10-05 20:25:54 -0600

    Se hicieron cambios en el diseño como la creación de la TopBar, el sistema de filtrado (Titulos, fecha de creacion y modivicación) en la ventana principal, se modifico la parte de visualización de notas y tareas para que que puedan visualizar sin necesidad de editar, pero en su topBar tiene para la edición y eliminación de la nota/tarea, se creo el formulario para la creación de las notas y tareas solocando radio buttoms, textfields y el botón que se va a encargar para la selección de archivos. Los topBars están personalizados a corde a la página, la página principal se agrego para el menú, el buscador y para configuración; en el de notas y tareas se coloco para regresar, editar y eliminar la nota; y en el del formulario unicamente tiene para regresar

--- app/src/main/java/com/example/proyectofinal/MainActivity.kt
+++ app/src/main/java/com/example/proyectofinal/MainActivity.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.proyectofinal
 
 import android.os.Bundle
@@ -11,7 +13,9 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+//import androidx.compose.foundation.layout.BoxScopeInstance.align
 import androidx.compose.foundation.layout.Column
+
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -21,16 +25,33 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -39,9 +60,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.proyectofinal.ui.theme.ProyectoFinalTheme
 import java.util.Date
 
@@ -62,102 +86,40 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+var Titulos = listOf("Terminar el diseño", "Jugar fortnite", "Jugar carreritas GTA")
+var Fechas = listOf("06-10-2024", "08-10-2024", "15-10-2024")
+
 @Composable
 fun PrincipalLayout(){
-    // Variable para tomar lo que tengamos en el campo de buscar
-    var buscarText by remember { mutableStateOf("") }
-
-    // Boton flotante para mandar a la otra pestaña
-    // Para que cree una nueva tarea
-    Box(modifier = Modifier.fillMaxWidth())
-    {
-        FloatingActionButton(
-            onClick = {},
-            modifier = Modifier
-                .padding(60.dp)
-                .align(Alignment.BottomEnd)
-                .size(70.dp)
-        ) {
-            Text("Botón")
+    Scaffold(
+        topBar =
+        {
+            MyTopBar()
+        },
+        floatingActionButton = {
+            BotonFlotante()
         }
-    }
-    // Listas de tareas (Es esqueleto por ahora xd)
-    Column(
-        modifier = Modifier
-            .padding(20.dp)
-            .fillMaxWidth()
-            .verticalScroll(rememberScrollState())
-    )
-    {
-        TextField(
-            value = buscarText,
-            onValueChange = { buscarText = it },
-            label = { Text("Buscar") },
+    ) { paddingValues ->
+        // Contenido principal de la pantalla
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-        )
-        // Nueva fila para elementos clicables en la parte superior, para notas y tareas
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(100.dp)
-                .padding(5.dp),
-            horizontalArrangement = Arrangement.Center
+                .padding(paddingValues)
+                .fillMaxSize()
         ) {
-            Text(
-                text = "Notas",
+            //var buscarText by remember { mutableStateOf("") }
+            Column(
                 modifier = Modifier
-                    .fillMaxWidth(0.5f)
-                    .fillMaxHeight()
-                    .padding(end = 5.dp)
-                    .clickable {}
-                    .border(
-                        BorderStroke(2.dp, Color.Black)
-                    )
-
-            )
-            Text(
-                text = "Tareas",
-                modifier = Modifier
+                    //.padding(20.dp)
                     .fillMaxWidth()
-                    .fillMaxHeight()
-                    .clickable {}
-                    .border(
-                        BorderStroke(2.dp, Color.Black)
-                    )
+                    .verticalScroll(rememberScrollState())
             )
+            {
+                FilterButton()
+                BoxTarea("Hacer el diseño", "06-10-2024")
+                BoxTarea("Jugar fornais", "08-10-2024")
+            }
         }
-        // Nueva fila para elementos clicables en la parte superior para pendientes y realizadas
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 16.dp, top = 16.dp),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            Text(
-                text = "Pendientes",
-                modifier = Modifier
-                    .fillMaxWidth(0.5f)
-                    .padding(end = 5.dp)
-                    .clickable {}
-                    .border(
-                        BorderStroke(2.dp, Color.Black)
-                    )
-
-            )
-            Text(
-                text = "Realizadas",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .clickable {}
-                    .border(
-                        BorderStroke(2.dp, Color.Black)
-                    )
-            )
-        }
-        BoxTarea("Tarea 1", "Terminar el diseño")
-        BoxTarea("Tarea 2", "Jugar fornais")
     }
 }
 
@@ -189,35 +151,137 @@ fun BoxTarea(
                 text = Fecha,
                 fontWeight = FontWeight.Light
             )
+            OutlinedButton(
+                onClick = {},
+                modifier = Modifier
+                    .padding(10.dp)
+                    .align(Alignment.CenterHorizontally),
+            )
+            {
+                Text("Ver")
+            }
         }
         Row(modifier = Modifier
-                .padding(20.dp)
+            .padding(top = 20.dp)
         )
         {
             // Boton para cancelar o borrar la tarea
-            Button(
+            OutlinedButton(
                 onClick = {},
                 modifier = Modifier
                     .padding(10.dp)
-                    .size(40.dp)
+                    //.size(40.dp)
             )
             {
-
+                //Icon(Icons.Default.Delete)
+                Text("\uD83D\uDDD1")
             }
             // Boton para terminar la tarea (Algo como para marcar que esta listo)
-            Button(
+            OutlinedButton(
                 onClick = {},
                 modifier = Modifier
                     .padding(10.dp)
-                    .size(40.dp)
+                    //.size(40.dp)
             )
             {
-
+                Text("✓")
             }
+
         }
+
         Spacer(modifier = Modifier.height(100.dp))
     }
 }
+
+@Composable
+fun MyTopBar() {
+    TopAppBar(
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                IconButton(onClick = { /* Acción */ }) {
+                    Icon(Icons.Filled.Menu, contentDescription = "Menu")
+                }
+                Text("Itsur Notes")
+            }
+        },
+        actions = {
+            IconButton(onClick = { /* Acción */ }) {
+                Icon(Icons.Filled.Search, contentDescription = "Buscar")
+            }
+            IconButton(onClick = { /* Acción */ }) {
+                Icon(Icons.Filled.Settings, contentDescription = "Configuracion")
+            }
+        }
+    )
+}
+
+
+
+@Composable
+fun BotonFlotante()
+{
+    FloatingActionButton(
+        onClick =  {},
+        containerColor = Color.Blue,
+        contentColor = Color.White
+    )
+    {
+        Icon(
+            imageVector = Icons.Default.Add,
+            contentDescription = "Agregar"
+        )
+    }
+
+}
+
+@Composable
+fun FilterButton() {
+    var expanded by remember { mutableStateOf(false) }
+    var selectedFilter by remember { mutableStateOf(0) }
+    val items = listOf("Titulo", "Fecha de creación", "Fecha de modificación")
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+    {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.End
+
+        ) {
+            IconButton(onClick = { expanded = true }) {
+                Icon(Icons.Filled.List, contentDescription = "Filtrado")
+            }
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = {expanded = false}
+            )
+            {
+                items.forEachIndexed(){index, item ->
+                    DropdownMenuItem(
+                        text = {Text(text = item)},
+                        onClick = { selectedFilter = index
+                        expanded = false
+                        })
+                }
+            }
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.End
+        )
+        {
+            Text(text = "Filtrando por: " + items[selectedFilter])
+        }
+    }
+
+}
+
+
+
 
 @Preview(showBackground = true)
 @Composable

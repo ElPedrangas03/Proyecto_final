d93e761e922f1dae7a49bf4abcbcfb85f59e82bf
Diego Jesus <s21120187@alumnos.itsur.edu.mx>
2024-11-28 02:08:41 -0600

    Me equivoque de proyecto he hice unos cambios pesados que tomarian mucho tiempo ponerlos en el actual

--- app/src/main/java/com/example/proyectofinal/alarmas/Alarma.kt
+++
@@ -1,9 +0,0 @@
-package com.example.proyectofinal.alarmas
-
-import java.time.LocalDateTime
-
-data class AlarmItem(
-    val idAlarma: String,
-    val alarmTime: String,
-    val message: String
-)
--- app/src/main/java/com/example/proyectofinal/alarmas/Scheduler.kt
+++
@@ -1,9 +0,0 @@
-package com.example.proyectofinal.alarmas
-
-import java.time.LocalDateTime
-
-interface AlarmScheduler {
-    fun schedule(alarmItem: AlarmItem)
-    fun cancel(alarmItem: AlarmItem)
-    fun edit(alarmItem: AlarmItem, newAlarmTime: LocalDateTime)
-}
